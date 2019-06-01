0800cee0d78bb85ec19c6e0db1b684791e41b8e4
franklad <frankbzd@gmail.com>
2019-05-31 21:09:10 -0400

    update

--- lab3/processForm.php
+++ lab3/processForm.php
@@ -17,16 +17,6 @@
 		}
 	}
 ?>
-<!DOCTYPE html>
-<!-- put your comments here-->
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-<head>
-	<meta charset="utf-8"/>
-	<title>Result</title>
-	<link rel="stylesheet" type="text/css" href="sharedStyles.css">
-	<!-- other head stuff -->
-</head>
-<body >
 	<?php
 		if(isset($err)){
 			echo "<p>".$err."</p>\n";
@@ -41,6 +31,3 @@
 	<?php
 		}
 	?>
-
-</body>
-</html>
