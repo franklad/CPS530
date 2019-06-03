8361db7e652f91f44d0f04ffffe4fba594849aec
franklad <frankbzd@gmail.com>
2019-06-03 19:32:05 -0400

    update

---
+++ lab4/L4.js
@@ -0,0 +1,20 @@
+function onLoad() {
+    document.getElementById("op").disabled = true;
+    document.getElementById("calc").disabled = true;
+    document.getElementById("add").disabled = true;
+}
+window.onload = () =>
+{
+    let opField = document.getElementById("op");
+    let numField = document.getElementById("number");
+    let addButton = document.getElementById("add");
+    let calcButton = document.getElementById("calc");
+    let clear = document.getElementById("clear");
+
+
+
+
+    function add() {
+        document.getElementById("formula");
+    }
+};
